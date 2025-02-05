6bf2bd615ea910c1c58b18f861ebf0795f2a97d3
seung-yuppy <songsy0107@gmail.com>
2025-02-05 09:12:12 +0900

    25/02/03 강의 끝

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
+// 아래와 같은 문법이 jsx이다
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <h1>이번에는 무조건 실행된다!!</h1>
+      </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,8 +4,21 @@ import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
+// JSX를 적용하지 않은 예시
+// const el = React.createElement('h1', {}, 'jsx를 쓰는걸까요?');
+
+// JSX를 적용하는 예시
+// const el = <h1>이것이 JSX이다</h1>;
+
+// JSX의 표현식
+// const test = "으갸갸걍";
+// const hello = test.length;
+// const el2 = <h1>리액트의 신기한 기능 {hello}개나 있어요!!</h1>
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // el2
   <React.StrictMode>
     <App />
   </React.StrictMode>
