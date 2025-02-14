49c44787b14394ec209c342504bd0a7de8d54f09
seung-yuppy <songsy0107@gmail.com>
2025-02-14 23:47:18 +0900

    25/02/14 강의

---
+++ src/basic/250214quiz/ProductItem.js
@@ -0,0 +1,11 @@
+// 예시 데이터
+const products = [
+    { id: 1, name: "노트북", price: 1000000, category: "전자기기" },
+    { id: 2, name: "티셔츠", price: 20000, category: "의류" }
+];
+
+function ProductItem() {
+
+}
+
+export default ProductItem;
